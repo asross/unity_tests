631330d9edd3884ee6cb7ca8814ff6eb1e1c3d95
Andrew Ross <andrewslavinross@gmail.com>
2013-11-03 09:06:04 -0500

    run tests by calling all methods matching naming convention. catch errors.

--- UnitTest.js
+++ UnitTest.js
@@ -4,41 +4,61 @@ import System.Text.RegularExpressions;
 class UnitTest extends MonoBehaviour {
   static var total;
   static var failures;
+  static var errors;
+  static var assertions;
 
   function Start() {
     if (!Application.isEditor) return;
+
     Debug.Log('** Running tests...');
+
     files = new DirectoryInfo("Assets/Scripts/Test").GetFiles("*_test.js"); // Edit these lines if your tests
     regex = new Regex("([a-zA-Z_]+_test)");                                 // live elsewhere or are named elsewise
-    total = failures = 0;
+    total = failures = errors = assertions = 0;
     for (file in files) {
       behaviour = regex.Match(file.ToString()).Groups[1].Value;
       if (behaviour) {
-        test_object = new GameObject();
-        test_object.AddComponent(behaviour);
-        test_object.SendMessage('RunTests');
-        Destroy(test_object);
+        test = gameObject.AddComponent(behaviour);
+        for (m in test.GetType().GetMethods())
+          if (m.Name.Contains('Test'))
+            Run(m, test);
+        Destroy(test);
       }
     }
-    Debug.Log('** ' + total + ' assertions, ' + failures + ' failures.');
+
+    Debug.Log('** ' + total + ' examples, ' + failures + ' failures, ' + errors + ' errors. ' + assertions + ' assertions.' );
+  }
+
+  function Run(method, test) {
+    total += 1;
+    try {
+      method.Invoke(test, null);
+    } catch(e : System.Reflection.TargetInvocationException) {
+      ee = e.InnerException;
+      if (ee.GetType() == Boo.Lang.Runtime.AssertionFailedException) {
+        failures += 1;
+      } else {
+        errors += 1;
+      }
+      Debug.LogException(ee);
+    }
   }
 
   static function Assert(expression, message) {
-    total += 1;
+    assertions += 1;
     if (!expression) {
-      failures += 1;
-      Debug.Log('  * Assertion Error' + (message ? ': ' + message : ''));
+      throw new Boo.Lang.Runtime.AssertionFailedException(message);
     }
   }
 
   /* Helpers */
-  static function AssertEqual(a, b, message)   { Assert((a == b), message); }
-  static function AssertUnequal(a, b, message) { Assert((a != b), message); }
   static function Assert(expression)           { Assert(expression, null);  }
-  static function AssertNot(expression, msg)   { Assert(!expression, msg);  }
-  static function AssertNot(expression)        { Assert(!expression);       }
   static function AssertEqual(a, b)            { Assert((a == b));          }
+  static function AssertEqual(a, b, message)   { Assert((a == b), message); }
   static function AssertUnequal(a, b)          { Assert((a != b));          }
-  static function That(expression, message)    { Assert(expression, msg);   }
+  static function AssertUnequal(a, b, message) { Assert((a != b), message); }
+  static function AssertNot(expression)        { Assert(!expression);       }
+  static function AssertNot(expression, msg)   { Assert(!expression, msg);  }
   static function That(expression)             { Assert(expression);        }
+  static function That(expression, msg)        { Assert(expression, msg);   }
 }
